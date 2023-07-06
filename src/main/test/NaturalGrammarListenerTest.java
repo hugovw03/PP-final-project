514b73dc40aac4bdd337c5303245415e63eb8b61
Justas Gvaziauskas <justas876ok@gmail.com>
2023-07-06 11:30:50 +0200

    committing some files
    pls commit

--- src/main/test/NaturalGrammarListenerTest.java
+++ src/main/test/NaturalGrammarListenerTest.java
@@ -10,16 +10,11 @@
 //import static org.junit.Assert.assertEquals;
 //import static org.junit.Assert.fail;
 //
-//
-//
-//
-//
 //import java.io.File;
 //import java.io.IOException;
 //
 //import org.antlr.v4.runtime.tree.ParseTree;
 //import org.junit.Test;
-//
 //
 //@SuppressWarnings("javadoc")
 //public class NaturalGrammarListenerTest {
